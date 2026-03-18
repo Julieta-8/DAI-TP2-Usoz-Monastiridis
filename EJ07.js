495ab807a8b4680b3f47fc11f637e1f1c5d2cb5b
Julieta-8 <48799592@est.ort.edu.ar>
2026-03-18 17:11:58 -0300

    EJ07

--- EJ07.js
+++ EJ07.js
@@ -1,15 +1,12 @@
-// librería https://jquery.com/
-import { $ } from "jquery";
-import {currency} from 'currency.js';
-let moneda = obtenerMoneda("AR");
+import currency from 'country-to-currency';
 
+
+let moneda = obtenerMoneda("AR");
 console.log(moneda);
+
+
 function obtenerMoneda(moneda){
-    const urlObj = new currency(moneda);
-    return {
-        host: urlObj.origin,
-        pathname: urlObj.pathname,
-        parametros: Object.fromEntries(urlObj.searchParams.entries())
-    };
+    return currency(moneda);
+   
 }
 
