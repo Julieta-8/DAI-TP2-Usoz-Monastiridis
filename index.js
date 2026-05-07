c2298d6aed18f47be806bdf56a5f5a1e21769e81
Julieta-8 <48799592@est.ort.edu.ar>
2026-05-07 08:48:16 -0300

    creación index

---
+++ index.js
@@ -0,0 +1,19 @@
+import { cambiarNombre } from './EJ01.js';
+import { parsearUrl } from './EJ02.js';
+import { obtenerMoneda } from './EJ03.js';
+import { cambiarNombre } from './EJ04.js';
+import { parsearUrl } from './EJ05.js';
+import { parsearUrl } from './EJ06.js';
+import { obtenerMoneda } from './EJ07.js';
+
+cambiarNombre('./entrada.txt', './salida.txt');
+
+console.log(
+    parsearUrl(
+        "http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo"
+    )
+);
+
+console.log(
+    obtenerMoneda("AR")
+);
